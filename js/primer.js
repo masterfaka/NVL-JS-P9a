6c7c5b08d18080ddb9856b0e41c98cbd66ec70c5
redwardPC <redwardweb@gmail.com>
2019-09-10 22:40:09 +0200

    Primer commit JSArrays

---
+++ js/primer.js
@@ -0,0 +1,7 @@
+//mostrar array y su respect elements
+var vector= new Array([1,2,3],["a","b","c"],[5,6,7,8],["A","B","C"]);
+console.log(vector);
+console.log("longitudTOtal: "+vector.length);
+console.log("logitudelemento 2:  "+ vector[1].length);
+console.log("3er element:"+vector[2])
+console.log("2ndo del 3ero: " + vector[2][1]);
